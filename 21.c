30f594905a3259f505efbe0d682426e9386e1a73
themedisons <58108744+themedisons@users.noreply.github.com>
2019-11-23 14:11:02 +0200

    Create 21.c

---
+++ 21.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <limits.h>
+int main()
+{
+ int a = 50000;
+ int b = 1000000;
+ int c = a * b
+; 
+ printf("int datu tipa izmers ir: %ld baiti \n", sizeof(int) );
+ printf("Apreekjinam a un b reizinaajumu:\n" );
+ printf("a = %d, b = %d \n", a, b);
+ printf("c = a * b = %d * %d = %d \n", a,b,c );
+} 

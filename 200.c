b461e4ce94224480d22b8722d68ee1afa5292b0f
themedisons <58108744+themedisons@users.noreply.github.com>
2019-11-23 14:11:41 +0200

    Create 200.c

---
+++ 200.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+
+int v;
+printf("Ievadiet veselu skaitli: \n");
+scanf("%d", &v);
+printf("Jus ievadijat %d skaitli \n", v);
+
+
+	
+for (int i=v;i<v+10;i++)
+{
+	printf("%d \n",i);
+}
+
+
+}
